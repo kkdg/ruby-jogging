d2d8eb7d527693b4cee7642b783a88c6e9977619
Kwag De Gi <code@skyaperture.com>
2015-01-23 13:49:58 +0900

    block in general

--- 0123/block.rb
+++ 0123/block.rb
@@ -7,3 +7,16 @@ def block
 end
 
 block {puts "You are in the block"}
+
+def test 
+	yield 5
+	puts "You are in the method test"
+	yield 100
+end
+
+test {|i| puts "You are in the block #{i}"}
+
+def test2(&block)
+	block.call
+end
+test2 { puts "Hi World!" }
---
+++ 0123/block2.rb
@@ -0,0 +1,9 @@
+END { 
+	puts "End of the code"
+}
+
+puts "Main code block"
+
+BEGIN {
+	puts "Begin of the code"
+}
