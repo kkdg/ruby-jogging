fe010a545acdecef2adf9d0da77a7442de7cc535
Kwag De Gi <code@skyaperture.com>
2015-02-02 21:24:48 +0900

    require module

---
+++ 0202/moral.rb
@@ -0,0 +1,10 @@
+
+module Moral
+	VERY_BAD = 0
+	BAD = 1
+	def Moral.sin(badness)
+
+	end
+end
+
+
---
+++ 0202/now.rb
@@ -0,0 +1,8 @@
+$LOAD_PATH << '.'
+
+require 'trig'
+require 'moral'
+
+y = Trig.sin(Trig::PI/4)
+
+
